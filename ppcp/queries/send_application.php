5290508c3b46badc83148a6cd845d0711f2794ef
Akindin <88840107+Akindin@users.noreply.github.com>
2023-05-24 14:17:42 +0300

    Fixed application sending

---
+++ ppcp/queries/send_application.php
@@ -0,0 +1,17 @@
+<?php
+require_once('./../config/connect.php');
+
+
+function getPOST($key) {
+    if(isset($_POST[$key])){
+        return $_POST[$key];
+    } else {
+        return null;
+    }
+}
+
+$query_string = "INSERT INTO `application` (`fullname`, `age`, `institute`, `course`, `phone`, `social_network`, `id_room`, `booking_start`, `booking_end`, `booking_date`,`application_date`) VALUES ('".getPOST('fullname')."', '".getPOST('age')."', '".getPOST('institute')."', '".getPOST('course')."', '".getPOST('phone')."', '".getPOST('social_network')."', '".getPOST('id_room')."', '".getPOST('booking_start')."', '".getPOST('booking_end')."', '".getPOST('booking_date')."', '".getPOST('application_date')."')";
+
+echo $query_string;
+mysqli_query($connect, $query_string);
+?>
