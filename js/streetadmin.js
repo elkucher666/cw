b0bca7903780d8d9768cc3ddbe6e9130c6596c90
elkucher666 <133093210+elkucher666@users.noreply.github.com>
2023-06-06 14:47:05 +0300

    add button room

---
+++ js/streetadmin.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", loadStreet);
+
+async function loadStreet() {
+
+    let response = await fetch('./../queries/get_room.php', {
+        method: 'POST'
+    });
+    let result = await response.text();
+    let rooms = JSON.parse(result);
+
+    let addresses = [];
+    for (let room of rooms) {
+        if (!addresses.includes(room.address)) {
+            addresses.push(room.address);
+        }
+    }
+    for (let address of addresses) {
+        let button = document.createElement("button");
+        button.textContent = address;
+        button.classList.add("tab");
+        document.querySelector(".addressses").append(button);
+
+
+        button.addEventListener("click", function() {
+
+            document.querySelectorAll(".tab").forEach(function (elem) {
+                elem.classList.remove("selected");
+            });
+            button.classList.add("selected");
+
+
+            room_elements.forEach(function(room) {
+                room.remove();
+            });
+            
+            let visible_rooms = room_elements.filter((room) => room.dataset.address == address);
+            visible_rooms.forEach(function(room) {
+                document.querySelector(".tabcontent").append(room);
+            });
+            
+        });
+    }
+    document.addEventListener('DOMContentLoaded', {
+        let timebutton = document.querySelector('button');
+        timebutton.addEventListener('click', function() {
+            document.querySelectorAll(".buttime").forEach(function (elem) {
+                elem.classList.remove("selected");
+            });
+            timebutton.classList.add("selected");
+
+
+            // room_elements.forEach(function(room) {
+            //     room.remove();
+            // });
+        });
+    );
+    }
+
+    
+}    
