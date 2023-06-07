27d265fc16314d68d08625e671ba7422a1b4a195
Akindin <88840107+Akindin@users.noreply.github.com>
2023-06-07 18:28:12 +0300

    admin change

--- js/streetadmin.js
+++ js/streetadmin.js
@@ -26,17 +26,6 @@ async function onLoad() {
                 elem.classList.remove("selected");
             });
             button.classList.add("selected");
-
-
-            room_elements.forEach(function(room) {
-                room.remove();
-            });
-            
-            let visible_rooms = room_elements.filter((room) => room.dataset.address == address);
-            visible_rooms.forEach(function(room) {
-                document.querySelector(".tabcontent").append(room);
-            });
-            
         });
     }
 
@@ -54,38 +43,43 @@ async function onLoad() {
     }
 
 
-    document.querySelector("#apply_filter").addEventListener("click", async function() {
-        let phone = document.querySelector("input[name='phone']");
-        let fullname = document.querySelector("input[name='fullname']");
-
-
-        if (phone.reportValidity() && fullname.reportValidity()) {
-            let booking_date = document.querySelector("input[name='booking_date']");
-            let application_date = document.querySelector("input[name='application_date']");
-            let approved = document.querySelector("select[name='approved']");
-            let address = document.querySelector("select[name='address']");
-
-            let formData = new FormData();
-
-            formData.append("phone", phone.value);
-            formData.append("fullname", fullname.value);
-            formData.append("booking_date", booking_date.value);
-            formData.append("application_date", application_date.value);
-            formData.append("approved", approved.value);
-            formData.append("address", address.value);
-
-            let response = await fetch('./../queries/get_filtered_application.php', {
-                method: 'POST',
-                body: formData
-            });
-
-            let result = await response.text();
-            console.log(result);
-            //let rooms = JSON.parse(result);
-
-        }
-
-
+    document.querySelector("#apply_filter").addEventListener("click", fetchApplications);
+    document.querySelectorAll(".buttime").forEach(function (button) {
+        button.addEventListener("click", fetchApplications);
     });
     
+}
+
+async function fetchApplications() {
+    let phone = document.querySelector("input[name='phone']");
+    let fullname = document.querySelector("input[name='fullname']");
+
+    let time_interval = document.querySelector(".buttime.selected")
+
+    if (phone.reportValidity() && fullname.reportValidity()) {
+        let booking_date = document.querySelector("input[name='booking_date']");
+        let application_date = document.querySelector("input[name='application_date']");
+        let approved = document.querySelector("select[name='approved']");
+        let address = document.querySelector("select[name='address']");
+
+        let formData = new FormData();
+
+        formData.append("phone", phone.value);
+        formData.append("fullname", fullname.value);
+        formData.append("booking_date", booking_date.value);
+        formData.append("application_date", application_date.value);
+        formData.append("approved", approved.value);
+        formData.append("address", address.value);
+        formData.append("time_interval", time_interval.value);
+
+        let response = await fetch('./../queries/get_filtered_application.php', {
+            method: 'POST',
+            body: formData
+        });
+
+        let result = await response.text();
+        let applications = JSON.parse(result);
+        console.log(applications);
+
+    }
 }
--- queries/get_filtered_application.php
+++ queries/get_filtered_application.php
@@ -1,13 +1,26 @@
 <?php
 require_once('./../config/connect.php');
 
-// $id = $_POST["id"];
 
 
-print_r($_POST);
+$query ="SELECT `application`.`id`, `application`.`booking_date`, `application`.`booking_start`, `application`.`booking_end`, `application`.`fullname`, `application`.`phone`, `room`.`address`, `room`.`name`, `application`.`application_date`, `application`.`approved` FROM `application`, `room` WHERE `application`.`id_room`=`room`.`id` ";
 
-// $query ="select `booking_date`, `booking_start`, `booking_end` from `application` where `id_room` = '$id' and `approved` = '1'";
-// $result = mysqli_fetch_all(mysqli_query($connect, $query), MYSQLI_ASSOC);
+$time_interval = $_POST["time_interval"];
 
-// print_r(json_encode($result));
+if ($time_interval == "today") {
+    $query .= 'AND `application`.`application_date` LIKE REPLACE(CONCAT(CURRENT_DATE), "-", ".")';
+} else if ($time_interval == "current_week") {
+    $query .= "AND WEEK(`application`.`application_date`,1) = WEEK(CURRENT_DATE,1)";
+} else if ($time_interval == "current_month") {
+    $query .= "AND `application`.`application_date` LIKE CONCAT('____-%',MONTH(CURRENT_DATE),'-%')";
+}
+
+
+
+$result = mysqli_fetch_all(mysqli_query($connect, $query), MYSQLI_ASSOC);
+
+$result["query"] = $query;
+
+print_r(json_encode($result));
+
 ?>
